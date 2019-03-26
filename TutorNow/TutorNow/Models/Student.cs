6cef3486b6b4ecabb014ff921eda685950d6177d
Luke Baierl <lukejb1984@hotmail.com>
2019-03-26 14:38:10 -0500

    Created student and tutor controllers, models and views with basic CRUD operations.

---
+++ TutorNow/TutorNow/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TutorNow.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        //May need to insert a Foreign Key for UserRolesID.
+        
+    }
+}
---
+++ TutorNow/TutorNow/Models/Tutor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TutorNow.Models
+{
+    public class Tutor
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        //May need Foreign Key UserID.
+    }
+}
