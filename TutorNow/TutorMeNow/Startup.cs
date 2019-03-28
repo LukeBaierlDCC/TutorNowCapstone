52847721a5ee9372905935033ab2932363819e25
Luke Baierl <lukejb1984@hotmail.com>
2019-03-28 13:09:55 -0500

    Fixing previous errors regarding database.

---
+++ TutorNow/TutorMeNow/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TutorMeNow.Startup))]
+namespace TutorMeNow
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
