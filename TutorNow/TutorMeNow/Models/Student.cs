05f4ef256dcfe5ebd3ef1f697a0a77aedd159bd8
Luke Baierl <lukejb1984@hotmail.com>
2019-03-28 13:33:14 -0500

    Worked subject class into Student model.

---
+++ TutorNow/TutorMeNow/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TutorMeNow.Models
+{
+    public enum Gender { Male,Female }
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+
+        public virtual ICollection<Subject> Subject { get; set; }
+    }
+    public class Subject
+    {
+        [Key]
+        public int SubjectId { get; set; }
+        [Required]
+        [Display(Name = "Course Subject")]
+    }
+}
