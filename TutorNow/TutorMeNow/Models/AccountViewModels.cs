49b5671cba0f5975e1245d15e84d6423c6a35536
Luke Baierl <lukejb1984@hotmail.com>
2019-04-01 09:50:39 -0500

    User now successfully able to register with user name. User name added to register view.

--- TutorNow/TutorMeNow/Models/AccountViewModels.cs
+++ TutorNow/TutorMeNow/Models/AccountViewModels.cs
@@ -69,8 +69,11 @@ namespace TutorMeNow.Models
 
     public class RegisterViewModel
     {
+        //[Required]
+        //public int SelectedRoleId { get; set; }
         [Required]
-        public int SelectedRoleId { get; set; } 
+        [Display(Name = "UserRole")]
+        public string UserRole { get; set; }
 
         [Required]
         [Display(Name = "UserName")]
@@ -81,10 +84,6 @@ namespace TutorMeNow.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
-
-        [Required]
-        [Display(Name = "UserRole")]
-        public string UserRole { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
