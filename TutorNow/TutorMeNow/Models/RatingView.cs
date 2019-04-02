86673d5225f9a1be18c60d03e6d8325e26b08dbe
Luke Baierl <lukejb1984@hotmail.com>
2019-04-02 15:07:43 -0500

    Added separate rating view model to assist in geographical search for tutor and student.

---
+++ TutorNow/TutorMeNow/Models/RatingView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TutorMeNow.Models
+{
+    public class RatingView
+    {
+        public Rating Rating { get; set; }
+        public Tutor Tutor { get; set; }
+        public List<Rating> Ratings { get; set; }
+    }
+}
